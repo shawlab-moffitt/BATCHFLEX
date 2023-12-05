54c219b3e733b69656ef1a74801acfc6cb81c404
ACSoupir <alex.soupir@jacks.sdstate.edu>
2023-12-05 16:32:44 -0500

    start of vignette for using package

--- R/batch_correction.R
+++ R/batch_correction.R
@@ -4,6 +4,7 @@
 #' @param meta Data frame of sample data with the first column being sample names that match the column names of the matrix
 #' @param batch.1 Column name from the meta file of the column that will be used for batch one information
 #' @param batch.2 Column name from the meta file of the column that will be used for batch two information
+#' @param log2_tranformed logical whether the data is alrady transformed
 #' @param treatment Column name from the meta file of the column that will be used for treatment information
 #' @param housekeeping Name of housekeeping gene set or character vector of housekeeping genes
 #' @param k Used in the RUVg method, the number of factors of unwanted variation to be estimated from the data
@@ -19,15 +20,35 @@
 #'
 #' @examples
 #' set.seed(101)
-batch_correction = function(mat = NULL,meta = NULL,method,batch.1 = NULL,batch.2 = NULL,treatment = NULL,housekeeping = NULL,
-                            k = 2,drop = 0,center = FALSE,round = FALSE,tolerance = 1e-8,par.prior = TRUE) {
-
+batch_correction = function(mat = NULL,
+                            meta = NULL,
+                            method,
+                            batch.1 = NULL,
+                            batch.2 = NULL,
+                            log2_tranformed = TRUE,
+                            treatment = NULL,
+                            housekeeping = NULL,
+                            k = 2,
+                            drop = 0,
+                            center = FALSE,
+                            round = FALSE,
+                            tolerance = 1e-8,
+                            par.prior = TRUE) {
+  #checks to make sure teh data is in the right format
   if (is.null(mat)) stop("Must provide matrix")
   if (!all(apply(mat,2,is.numeric)) | !is(mat,"matrix")) stop("Must be numeric matrix")
   if (is.null(meta)) stop("Must provide meta data")
   if (!all(method %in% c("Limma", "ComBat", "Mean Centering", "ComBatseq", "Harman", "RUVg"))) stop("Batch correction methods not found")
   #if (!is.null(housekeeping)) stop("Must provide name of housekeeping gene set or vector of housekeeping genes")
-
+  if(method == "all") method = c("Limma", "ComBat", "Mean Centering", "ComBatseq", "Harman", "RUVg")
+  if(!is.null(batch.1))
+    if(!(batch.1 %in% colnames(meta)))
+        stop("batch.1 needs to be a column name in the metadata")
+  if(length(batch.1) > 1) stop('batch.1 can only be of length 1 or NULL')
+  if(!is.null(batch.2))
+    if(!(batch.2 %in% colnames(meta)))
+      stop("batch.2 needs to be a column name in the metadata")
+  if(length(batch.2) > 1) stop('batch.2 can only be of length 1 or NULL')
 
   meta <- meta[match(colnames(mat), meta[[1]]),]
   batch_corrected_list <- list()
@@ -38,34 +59,35 @@ batch_correction = function(mat = NULL,meta = NULL,method,batch.1 = NULL,batch.2
       total_covariates <- paste0(treatment,collapse = "+")
       model_matrix <- stats::model.matrix(reformulate(total_covariates), data = as.data.frame(meta))
     }
-
+    #get the values associated with batch 1 and batch 2
+    #input for 'removeBatchEffect is vectors
+    batch.1 = if(is.null(batch.1)) NULL else {meta[,batch.1]}
+    batch.2 = if(is.null(batch.2)) NULL else {meta[,batch.2]}
     batch_corrected_list$limma_corrected <- limma::removeBatchEffect(
-      mat,
-      batch = meta[,batch.1],
-      batch2 = NULL,
+      if(!log2_tranformed) log2(mat) else {mat},
+      batch = batch.1,
+      batch2 = batch.2,
       covariates = model_matrix
     )
 
   }
   if("ComBat" %in% method){
-    if (!is.null(batch.1)) {
-      batch_combat <- meta[,batch.1]
-      modcombat <-  stats::model.matrix(~1, data = as.data.frame(meta))
-      batch_corrected_list$ComBat_corrected <- sva::ComBat(
-        dat = mat,
-        batch = batch_combat,
-        mod = modcombat,
-        par.prior = par.prior
-      )
-    }
+    if (is.null(batch.1)) stop("ComBat requires a batch.1 input")
+    batch_combat <- meta[,batch.1]
+    modcombat <-  stats::model.matrix(~1, data = as.data.frame(meta))
+    batch_corrected_list$ComBat_corrected <- sva::ComBat(
+      dat = if(!log2_tranformed) log2(mat) else {mat},
+      batch = batch_combat,
+      mod = modcombat,
+      par.prior = par.prior
+    )
   }
   if("Mean Centering" %in% method){
-    if (!is.null(batch.1)) {
-      mean_centering_batch = meta[,batch.1]
-      mean_centering_data = t(mat)
-      mean_center <- bapred::meancenter(as.matrix(mean_centering_data), as.factor(mean_centering_batch))
-      batch_corrected_list$mean_center_correction <- as.data.frame(t(mean_center$xadj))
-    }
+    if (is.null(batch.1)) stop("Mean Centering requires a batch.1 input")
+    mean_centering_batch = meta[,batch.1]
+    mean_centering_data = t(if(!log2_tranformed) log2(mat) else {mat})
+    mean_center <- bapred::meancenter(as.matrix(mean_centering_data), as.factor(mean_centering_batch))
+    batch_corrected_list$mean_center_correction <- as.data.frame(t(mean_center$xadj))
   }
   if("ComBatseq" %in% method){
     if (is.null(treatment)){
@@ -75,7 +97,7 @@ batch_correction = function(mat = NULL,meta = NULL,method,batch.1 = NULL,batch.2
       model_matrix <- stats::model.matrix(reformulate(total_covariates), data = as.data.frame(meta))
     }
     combatseq_corrected <- sva::ComBat_seq(
-      (2^mat),
+      if(!log2_tranformed) log2(mat) else {mat},
       batch = meta[,batch.1],
       covar_mod = model_matrix
     )
@@ -83,7 +105,7 @@ batch_correction = function(mat = NULL,meta = NULL,method,batch.1 = NULL,batch.2
   }
   if("Harman" %in% method){
     harman_correction_PCA <- Harman::harman(
-      mat,
+      if(!log2_tranformed) log2(mat) else {mat},
       expt = meta[,treatment],
       batch = meta[,batch.1],
       limit = 0.95,
@@ -92,8 +114,10 @@ batch_correction = function(mat = NULL,meta = NULL,method,batch.1 = NULL,batch.2
     batch_corrected_list$harman_corrected <- Harman::reconstructData(harman_correction_PCA)
   }
   if("RUVg" %in% method){
+    h_i_m = housekeeping %in% row.names(mat)
+    message(sum(h_i_m), " genes of ", length(h_i_m), " found in data")
     RUVg_correction <- RUVSeq::RUVg(
-      mat,
+      if(!log2_tranformed) log2(mat) else {mat},
       cIdx = housekeeping,
       k = k,
       drop = drop,
