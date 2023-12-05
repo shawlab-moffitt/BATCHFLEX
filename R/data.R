bb86377024c0c10b1e7e3f7d0de8387256b48804
ACSoupir <alex.soupir@jacks.sdstate.edu>
2023-12-05 12:13:25 -0500

    added house keeping genes

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Eisenberg's housekeeping genes
+#'
+#' @format ## `eisenbert`
+#' a vector with house keeping genes
+#'
+#' @source <https://pubmed.ncbi.nlm.nih.gov/23810203/>
+"eisenberg"
+
+#' Eisenberg's housekeeping genes
+#'
+#' @format ## `hsaio`
+#' a vector with house keeping genes
+#'
+#' @source <https://www.gsea-msigdb.org/gsea/msigdb/cards/HSIAO_HOUSEKEEPING_GENES>
+"hsaio"
+
+#' Eisenberg's housekeeping genes
+#'
+#' @format ## `lin500`
+#' a vector with house keeping genes
+#'
+#' @source <https://pubmed.ncbi.nlm.nih.gov/23810203/>
+"lin500"
+
---
+++ data-raw/process_housekeeping_genes.R
@@ -0,0 +1,8 @@
+#eisenberg = read.csv("data-raw/eisenberg.txt")[[1]]
+usethis::use_data(eisenberg, overwrite = TRUE)
+
+#hsaio = read.csv('data-raw/hsiao.txt')[[1]]
+usethis::use_data(hsaio, overwrite = TRUE)
+
+#lin500 = read.csv('data-raw/lin500.txt')[[1]]
+usethis::use_data(lin500, overwrite = TRUE)
