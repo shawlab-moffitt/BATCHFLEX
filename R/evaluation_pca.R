16b0f015ac8219e1fd937260131daa8665a7e2fa
JDDev3 <JTD0020@auburn.edu>
2023-12-28 18:30:21 -0500

    added "all" to pca evaluation function

--- R/evaluation_pca.R
+++ R/evaluation_pca.R
@@ -8,10 +8,11 @@ evaluation_pca = function(mat = NULL,
                           variable_of_interest = NULL,
                           color_by = "batch"){
   pca_plot_list <- list()
+  if("all" %in% annotation) annotation = c("cluster", "meta")
   if (!all(annotation %in% c("cluster", "meta"))){
     stop("Annotation method not found")
   }
-  if (annotation == "cluster" & is.null(uncorrected_cluster_number) ){
+  if ("cluster" %in% annotation & is.null(uncorrected_cluster_number) ){
     uncorrected_dunn_k <- c(2:10)
     uncorrected_dunnin <- c()
     for (i in dunn_k){
@@ -24,7 +25,7 @@ evaluation_pca = function(mat = NULL,
     colnames(uncorrected_dunn_index_analysis) <- c("cluster_number", "dunn_index")
     uncorrected_cluster_number = uncorrected_dunn_index_analysis$cluster_number[which(max(uncorrected_dunn_index_analysis$dunn_index) == uncorrected_dunn_index_analysis$dunn_index)]
   }
-  if (annotation == "cluster" & !is.null(batch_correction) & is.null(corrected_cluster_number) ){
+  if ("cluster" %in% annotation & !is.null(batch_correction) & is.null(corrected_cluster_number) ){
     corrected_dunn_k <- c(2:10)
     corrected_dunnin <- c()
     for (i in dunn_k){
@@ -37,38 +38,38 @@ evaluation_pca = function(mat = NULL,
     colnames(corrected_dunn_index_analysis) <- c("cluster_number", "dunn_index")
     corrected_cluster_number = corrected_dunn_index_analysis$cluster_number[which(max(corrected_dunn_index_analysis$dunn_index) == corrected_dunn_index_analysis$dunn_index)]
   }
-  if (annotation == "meta" & is.null(batch)){
+  if ("meta" %in% annotation & is.null(batch)){
     print("Missing batch information")
   }
-  if (annotation == "meta" & is.null(variable_of_interest)){
+  if ("meta" %in% annotation & is.null(variable_of_interest)){
     print("Missing variable of interest")
   }
-  if (annotation == "cluster"){
+  if ("cluster" %in% annotation){
     uncorrected_PCA <- cluster::pam(as.data.frame(t(mat)), uncorrected_cluster_number)
-    pca_plot_list$uncpca <- ggplot2::autoplot(
+    pca_plot_list$uncpcaclust <- ggplot2::autoplot(
       uncorrected_PCA,
       frame = TRUE,
       frame.type = "norm")
     if (!is.null(batch_correction)){
       corrected_PCA <- cluster::pam(as.data.frame(t(batch_correction)), corrected_cluster_number)
-      pca_plot_list$corpca <- ggplot2::autoplot(
+      pca_plot_list$corpcaclust <- ggplot2::autoplot(
         corrected_PCA,
         frame = TRUE,
         frame.type = "norm")
     }
   }
-  if (annotation == "meta"){
+  if ("meta" %in% annotation){
     uncorrected_PCA <- stats::prcomp(t(mat), scale. = TRUE)
     uncorrected_PCA_data <- cbind(t(mat), meta)
     if (color_by == "batch"){
-      pca_plot_list$uncpca <- ggplot2::autoplot(
+      pca_plot_list$uncpcameta <- ggplot2::autoplot(
         uncorrected_PCA,
         uncorrected_PCA_data,
         color = batch,
         shape = variable_of_interest
       )
     }else if (color_by == "voi"){
-      pca_plot_list$uncpca <- ggplot2::autoplot(
+      pca_plot_list$uncpcameta <- ggplot2::autoplot(
         uncorrected_PCA,
         uncorrected_PCA_data,
         color = variable_of_interest,
@@ -79,14 +80,14 @@ evaluation_pca = function(mat = NULL,
       corrected_PCA <- stats::prcomp(t(batch_correction), scale. = TRUE)
       corrected_PCA_data <- cbind(t(batch_correction), meta)
       if (color_by == "batch"){
-        pca_plot_list$corpca <- ggplot2::autoplot(
+        pca_plot_list$corpcameta <- ggplot2::autoplot(
           corrected_PCA,
           corrected_PCA_data,
           color = batch,
           shape = variable_of_interest
         )
       }else if (color_by == "voi"){
-        pca_plot_list$corpca <- ggplot2::autoplot(
+        pca_plot_list$corpcameta <- ggplot2::autoplot(
           corrected_PCA,
           corrected_PCA_data,
           color = variable_of_interest,
